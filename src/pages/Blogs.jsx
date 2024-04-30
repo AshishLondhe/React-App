6bd23c6a729f40d64e4b5dfc62b992cc5136c8ec
londheashish323@gmail.com <141058344+AshishLondhe@users.noreply.github.com>
2024-05-01 00:59:17 +0530

    router added

---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Blogs() {
+  return (
+    <div>Blogs
+        This is my first blog
+    </div>
+  )
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/style.css"
+function Contact() {
+  return (
+    <div className='first'>Contact
+
+        hkhhhbkhb
+    </div>
+  )
+}
+
+export default Contact
